408e08819f6d9d414c1f46bfa25bda6cf2a33c78
rolphy <martinrh83@gmail.com>
2015-01-26 15:35:46 -0300

    Primer Commit

---
+++ src/controlador/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import vistas.Principal;
+
+/**
+ *
+ * @author martinrh83
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Principal window = new Principal();
+        window.setVisible(true);
+    }
+    
+}
